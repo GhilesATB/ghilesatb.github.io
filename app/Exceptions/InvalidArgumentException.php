ecb3053f9f12b3551a851a86976a9b8360edfc8d
aghiles1592 <aghiles1592@gmail.com>
2023-02-25 17:41:06 +0100

    Implement invalidArgumentException

---
+++ app/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Exceptions;
+
+class InvalidArgumentException extends BaseException
+{
+    public const MSG_PREFIX = "Argument Error";
+}
--- app/Services/DataSource/MediaDataSource.php
+++ app/Services/DataSource/MediaDataSource.php
@@ -2,9 +2,13 @@
 
 namespace App\Services\DataSource;
 
+use App\Enums\Terms;
 use App\Exceptions\DataSourceException;
-use Illuminate\Support\Facades\Http;
+use App\Exceptions\InvalidArgumentException;
 use Exception;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
+
 class MediaDataSource implements ExternalApiDataSourceInterface
 {
     public function __construct(private readonly string $base_url)
@@ -14,7 +18,7 @@ class MediaDataSource implements ExternalApiDataSourceInterface
     /*
      * build and request data from external Api based on term media_type , ids and request parameters
      */
-    public function getData(string $term, string $mediaType, array $id = null, $params = null): Object
+    public function getData(string $term, string $mediaType, array $id = null, $params = null): object
     {
         $uri = $this->makeUri($term, $mediaType, $id);
         $data = $this->RequestData($uri, $params);
@@ -25,11 +29,10 @@ class MediaDataSource implements ExternalApiDataSourceInterface
     /*
      * request data from external api based on uri and request params using authorization token
      */
-    public function RequestData(string $uri, array $params): Object
+    public function RequestData(string $uri, array $params): object
     {
         try {
-            $response = Http::withHeaders(['Authorization' =>'Bearer ' .env('TMDB_API_TOKEN')])->get($uri, $params);
-
+            $response = Http::withHeaders(['Authorization' => 'Bearer ' . env('TMDB_API_TOKEN')])->get($uri, $params);
             $data = json_decode($response->body());
 
             return $data;
@@ -49,6 +52,8 @@ class MediaDataSource implements ExternalApiDataSourceInterface
             'search' => "{$this->base_url}/search/{$mediaType}",
             'detail' => "{$this->base_url}/{$mediaType}/{$ids[$mediaType]}",
             'videos' => "{$this->base_url}/{$mediaType}/{$ids[$mediaType]}/videos",
+
+            default => throw new InvalidArgumentException('the given value is invalid', Response::HTTP_BAD_REQUEST)
         };
 
         return $uri;
