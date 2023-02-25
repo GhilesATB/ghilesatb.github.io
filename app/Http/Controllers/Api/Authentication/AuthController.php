5406a310d7d714d1971d098a2d82e12870db69b7
GhilesATB <113276997+GhilesATB@users.noreply.github.com>
2023-02-25 19:07:54 +0100

    Refactor auth (#13)
    * code refactoring
    
    * code update
    
    * refactor auth
    
    ---------
    
    Co-authored-by: aghiles1592 <aghiles1592@gmail.com>

--- app/Http/Controllers/Api/Authentication/AuthController.php
+++ app/Http/Controllers/Api/Authentication/AuthController.php
@@ -2,32 +2,17 @@
 
 namespace App\Http\Controllers\Api\Authentication;
 
-use App\Exceptions\AuthFailException;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Authentification\LoginRequest;
 use App\Http\Requests\Authentification\RegisterRequest;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request): Response
-    {
-        if (!Auth::attempt($request->only(['name', 'password']))) {
-            throw new AuthFailException('wrong credentials', Response::HTTP_UNAUTHORIZED);
-        }
-
-        $user = Auth::user();
-        $token = $user->createToken("user")->plainTextToken;
-        $cookie = cookie('token', $token, 3600);
-
-        return response()->json(['token' => $token], 200)->withCookie($cookie);
-    }
-
-
-    public function register(RegisterRequest $request): Response
+    public function register(RegisterRequest $request): JsonResponse
     {
         $user = User::create(
             array_merge(
@@ -37,6 +22,10 @@ class AuthController extends Controller
             )
         );
 
-        return response($user, Response::HTTP_OK);
+        Auth::login($user);
+        $token = $user->createToken("user")->plainTextToken;
+        $cookie = cookie('token', $token, 3600);
+
+        return response()->json(['token' => $token], Response::HTTP_OK)->withCookie($cookie);
     }
 }
