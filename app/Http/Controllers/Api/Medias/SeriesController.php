cb24ec059a2f372ca774a931bce64fdcc61095e6
GhilesATB <113276997+GhilesATB@users.noreply.github.com>
2023-02-25 17:47:01 +0100

    media refactore media endpoints (#11)
    Co-authored-by: aghiles1592 <aghiles1592@gmail.com>

--- app/Http/Controllers/Api/Medias/SeriesController.php
+++ app/Http/Controllers/Api/Medias/SeriesController.php
@@ -4,41 +4,44 @@ namespace App\Http\Controllers\Api\Medias;
 
 use App\Http\Controllers\Controller;
 use App\Services\MediaService\MediaService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 
 class SeriesController extends Controller
 {
-    public function index(MediaService $mediaService)
+    const MEDIA = 'tv';
+
+    public function index(MediaService $mediaService): JsonResponse
     {
-        $data = $mediaService->getPage('tv', request()->query->all());
+        $data = $mediaService->getPage(self::MEDIA, request()->query->all());
 
         return response()->json($data, Response::HTTP_OK);
     }
 
-    public function topRated(MediaService $service)
+    public function topRated(MediaService $service): JsonResponse
     {
-        $slice = $service->getTopRated('tv', request()->query->all());
+        $slice = $service->getTopRated(self::MEDIA, request()->query->all());
 
         return response()->json($slice, Response::HTTP_OK);
     }
 
-    public function search(MediaService $service)
+    public function search(MediaService $service): JsonResponse
     {
-        $slice = $service->search('tv', request()->query->all());
+        $slice = $service->search(self::MEDIA, request()->query->all());
 
         return response()->json($slice, Response::HTTP_OK);
     }
 
-    public function detail(string $id, MediaService $service)
+    public function detail(string $id, MediaService $service): JsonResponse
     {
-        $slice = $service->detail('tv', ['tv' => $id], request()->query->all());
+        $slice = $service->detail(self::MEDIA, [self::MEDIA => $id], request()->query->all());
 
         return response()->json($slice, Response::HTTP_OK);
     }
 
-    public function videos(string $id, MediaService $service)
+    public function videos(string $id, MediaService $service): JsonResponse
     {
-        $slice = $service->videos('tv', ['tv' => $id], request()->query->all());
+        $slice = $service->videos(self::MEDIA, [self::MEDIA => $id], request()->query->all());
 
         return response()->json($slice, Response::HTTP_OK);
     }
